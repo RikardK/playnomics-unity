c99580aafc3a2b621fab56cb0ae70f6d22c0cd1e
Jared Jenkins <jared@playnomics.com>
2013-12-11 18:10:21 -0800

    Modify the delegate so that it calls the right object and methods

--- src/android/Unity/src/com/playnomics/android/unity/PlacementDelegate.java
+++ src/android/Unity/src/com/playnomics/android/unity/PlacementDelegate.java
@@ -4,26 +4,25 @@ import com.playnomics.android.sdk.IPlaynomicsPlacementRawDelegate;
 import com.unity3d.player.UnityPlayer;
 
 public class PlacementDelegate implements IPlaynomicsPlacementRawDelegate {
-	
-	private final String GAME_OBEJECT_NAME = "Playnomics.PlacementDelegateReceiver";
+	private final String GAME_OBEJECT_NAME = "Playnomics";
 
 	@Override
 	public void onClose(String rawJsonData) {
-		UnityPlayer.UnitySendMessage(GAME_OBEJECT_NAME, "onClose", rawJsonData);
+		UnityPlayer.UnitySendMessage(GAME_OBEJECT_NAME, "OnClose", rawJsonData);
 	}
 
 	@Override
 	public void onRenderFailed() {
-		UnityPlayer.UnitySendMessage(GAME_OBEJECT_NAME, "onRenderFailed", null);
+		UnityPlayer.UnitySendMessage(GAME_OBEJECT_NAME, "OnRenderFailed", null);
 	}
 
 	@Override
 	public void onShow(String rawJsonData) {
-		UnityPlayer.UnitySendMessage(GAME_OBEJECT_NAME, "onShow", rawJsonData);
+		UnityPlayer.UnitySendMessage(GAME_OBEJECT_NAME, "OnShow", rawJsonData);
 	}
 
 	@Override
 	public void onTouch(String rawJsonData) {
-		UnityPlayer.UnitySendMessage(GAME_OBEJECT_NAME, "onTouch", rawJsonData);
+		UnityPlayer.UnitySendMessage(GAME_OBEJECT_NAME, "OnTouch", rawJsonData);
 	}
 }
