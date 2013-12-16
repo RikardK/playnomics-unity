d7d45d25aff1b708cde7889cd0539714c5ec2f19
Jared Jenkins <jared@playnomics.com>
2013-12-16 15:57:23 -0800

    Add the Plugin files for the Unity Asset file

---
+++ src/unity/PlaynomicsPlugin/PlaynomicsPlugin/Assets/TestScenes/Integration.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using Playnomics.Unity;
+
+public class Integration : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		const long appId = 733674050111288021L;
+		PN.StartSDK(appId);
+		PN.PreloadPlacements("http", "ad", "noShow", "json", "nullTarget");
+		PN.PlacementDelegate = new PlacementDelegate();
+	}
+	
+
+	// Update is called once per frame
+	void Update() 
+	{
+	}
+
+	void OnGUI()
+	{
+		GUI.depth = 2;
+
+		int numberButtons = 8;
+
+		float height = Screen.height/10;
+		float width = Screen.width * 0.50f;
+		
+		float verticalPadding = 10;
+		
+		float x = (Screen.width - width)/2;
+		float y = 0;
+
+		if(GUI.Button(new Rect(x, y += (verticalPadding), width, height), "Attribute Install"))
+		{
+			string source = "source";
+			string campaign = "campaign";
+			DateTime installDate = DateTime.UtcNow;
+			PN.AttributeInstall(source, campaign, installDate);
+		}
+		
+		if(GUI.Button(new Rect(x, y += (verticalPadding + height), width, height), "Transaction"))
+		{
+			float price = 0.99f;
+			int quantity = 1;
+			PN.TransactionInUSD(price, quantity);
+		}
+				
+		if(GUI.Button(new Rect(x, y += (verticalPadding + height), width, height), "App Start Milestone"))
+		{
+			PN.CustomEvent("app start");
+		}
+
+		if(GUI.Button(new Rect(x, y += (verticalPadding + height), width, height), "HTTP"))
+		{
+			PN.ShowPlacement("http");
+		}
+
+		if(GUI.Button(new Rect(x, y += (verticalPadding + height), width, height), "Ads"))
+		{
+			PN.ShowPlacement("ad");
+		}
+
+		if(GUI.Button(new Rect(x, y += (verticalPadding + height), width, height), "Null Target"))
+		{
+			PN.ShowPlacement("nullTarget");
+		}
+
+		if(GUI.Button(new Rect(x, y += (verticalPadding + height), width, height), "JSON"))
+		{
+			PN.ShowPlacement("json");
+		}
+
+		if(GUI.Button(new Rect(x, y += (verticalPadding + height), width, height), "No Show"))
+		{
+			PN.ShowPlacement("noShow");
+		}
+	}
+}
---
+++ src/unity/PlaynomicsPlugin/PlaynomicsPlugin/Assets/TestScenes/PlacementDelegate.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using Playnomics.Unity;
+using Playnomics.LitJson;
+
+public class PlacementDelegate : IPlacementDelegate 
+{
+
+	public void OnShow(JsonData rawData)
+	{
+		//just post the message to Android Activity
+
+		string message = string.Format("OnShow {0}", rawData == null ? string.Empty : JsonMapper.ToJson(rawData));
+		using(var shimClass = new AndroidJavaClass("com.playnomics.android.unity.PlaynomicsShim"))
+		{
+			shimClass.CallStatic("toastMessage", new object[1] { message });
+		}
+	}
+	
+	public void OnTouch(JsonData rawData)
+	{
+		//just post the message to Android Activity
+		string message = string.Format("OnTouch {0}", rawData == null ? string.Empty : JsonMapper.ToJson(rawData));
+		using(var shimClass = new AndroidJavaClass("com.playnomics.android.unity.PlaynomicsShim"))
+		{
+			shimClass.CallStatic("toastMessage", new object[1] { message });
+		}
+	}
+	
+	public void OnClose(JsonData rawData)
+	{
+		//just post the message to Android Activity
+		string message = string.Format("OnClose {0}", rawData == null ? string.Empty : JsonMapper.ToJson(rawData));
+		using(var shimClass = new AndroidJavaClass("com.playnomics.android.unity.PlaynomicsShim"))
+		{
+			shimClass.CallStatic("toastMessage", new object[1] { message });
+		}
+	}
+	
+	public void OnRenderFailed()
+	{
+		using(var shimClass = new AndroidJavaClass("com.playnomics.android.unity.PlaynomicsShim"))
+		{
+			shimClass.CallStatic("toastMessage", new object[1] { "OnRenderFailed" });
+		}	
+	}
+}
