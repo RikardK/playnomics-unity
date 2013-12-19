43726b82f274acc8fe81ff166ec01232dd410c6f
Jared Jenkins <jared@playnomics.com>
2013-12-19 14:35:23 -0800

    Update the Unity plugin dependencies

--- src/unity/PlaynomicsPlugin/PlaynomicsPlugin/Assets/TestScenes/Integration.cs
+++ src/unity/PlaynomicsPlugin/PlaynomicsPlugin/Assets/TestScenes/Integration.cs
@@ -12,7 +12,6 @@ public class Integration : MonoBehaviour {
 		PN.PreloadPlacements("http", "ad", "noShow", "json", "nullTarget");
 		PN.PlacementDelegate = new PlacementDelegate();
 	}
-	
 
 	// Update is called once per frame
 	void Update() 
